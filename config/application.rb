d5acea9c433f4a75d377aa47af0a57fbd08e8728
gittaiki <0215.taiki@gmail.com>
2022-04-04 23:40:59 +0900

    Add: 不要なファイルを作成しないように設定

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,12 @@ module Yudaneko
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
