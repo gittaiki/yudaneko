112d76dfeeb5eb883c0c917390863f8d859453f8
gittaiki <0215.taiki@gmail.com>
2022-04-08 21:54:30 +0900

    Fix: troublesコントローラの処理をモデルに移す

--- app/controllers/troubles_controller.rb
+++ app/controllers/troubles_controller.rb
@@ -1,8 +1,8 @@
 class TroublesController < ApplicationController
   def index
     @buy_trouble = Trouble.find(1)
-    @buy_question = Question.where(trouble_id: 1).sample
+    @buy_question = Question.first_question(@buy_trouble)
     @eat_trouble = Trouble.find(2)
-    @eat_question = Question.where(trouble_id: 2).sample
+    @eat_question = Question.first_question(@eat_trouble)
   end
 end
