af1817a7554d0b0ce9deffc83850b1fc7da5e754
gittaiki <0215.taiki@gmail.com>
2022-04-08 22:35:39 +0900

    Add: 質問で「いいえ」を押したときの処理を実装#12

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
+  get 'cannot', to: 'static_pages#cannot'
   resources :troubles, only: %i[index] do
     resources :questions, only: %i[show]
   end
