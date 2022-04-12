3c4c7d5046a47a62133c14588a8f21f3e2f88258
gittaiki <0215.taiki@gmail.com>
2022-04-12 23:11:34 +0900

    Add: 利用規約とプライバシーポリシーと問い合わせを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'troubles#index'
-  get 'cannot', to: 'static_pages#cannot'
+  get 'terms', to: 'static_pages#terms'
+  get 'policy', to: 'static_pages#policy'
   resources :troubles, only: %i[index] do
     resources :questions, only: %i[show]
   end
