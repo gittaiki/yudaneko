784219b0896ce426bb1cb6809864f2f1b4c19a6a
gittaiki <0215.taiki@gmail.com>
2022-04-16 10:26:25 +0900

    Add: metaタグを設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  # ngrokを使ったアクセスを許可する設定
+  config.hosts << '.ngrok.io'
 end
